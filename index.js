09dcd2b9465bb03d7591f7a6f0589d6d9e37ba4b
Michael Peter Rixon <michaelpeterrixon@Michaels-MacBook-Air.local>
2022-04-25 18:02:42 +1000

    Cards can rotate

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 console.log("hello");
+
+
+let playCard = document.querySelectorAll(".playCard");
+let frontCard = document.querySelectorAll(".frontCard");
+let backCard = document.querySelectorAll(".backCard");
+
+
+for (let i = 0; i < playCard.length; i++) {
+  let pCard = playCard[i];
+  let fCard = frontCard[i];
+  let bCard = backCard[i];
+
+  pCard.addEventListener("click", function (e) {
+    pCard.classList.toggle('is-flipped');
+    bCard.classList.toggle('back-invisible');
+    let i = fCard.getAttribute('z-index');
+    console.log(i);
+    if (i > 0) {
+      fCard.classList.toggle('back-invisible');
+      fCard.setAttribute('z-index', -1 * i);
+    }
+  });
+}
+
